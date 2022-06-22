b5aaa06becc0522124d379e541ea6fa650c8fd27
Case Walker <case.walker@feedzai.com>
2022-06-22 08:29:10 -0400

    Support other mods in creating a NarratorMode mixin

---
+++ src/main/java/com/casewalker/modutils/util/NarratorModeMixinHelperParent.java
@@ -0,0 +1,151 @@
+/*
+ * Licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2022 Case Walker.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.casewalker.modutils.util;
+
+import net.minecraft.client.option.NarratorMode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * This class should be extended inside a mixin which targets the {@link NarratorMode} enum. To add new mode(s), the
+ * implementer should create a mixin class which implements this class privately and then adds modes, such as:
+ *
+ * <pre>{@code
+ *   @Mixin(NarratorMode.class)
+ *   @Unique
+ *   public abstract class MyNarratorModeMixin {
+ *
+ *       private static class NarratorModeMixinHelper extends NarratorModeMixinHelperParent {
+ *           @Override
+ *           public NarratorMode narratorModeInvokeInit(
+ *                   final String internalName,
+ *                   final int internalId,
+ *                   final int id,
+ *                   final String name) {
+ *               return MyNarratorModeMixin.narratorModeInvokeInit(internalName, internalId, id, name);
+ *           }
+ *       }
+ *
+ *       @Shadow
+ *       @Final
+ *       @Mutable
+ *       private static NarratorMode[] field_18183;
+ *
+ *       @Shadow
+ *       @Final
+ *       @Mutable
+ *       private static NarratorMode[] VALUES;
+ *
+ *       private final static NarratorModeMixinHelper HELPER = new NarratorModeMixinHelper();
+ *
+ *       private static final NarratorMode EXAMPLE1 =
+ *               addNarratorMode("EXAMPLE1", 4, "options.narrator.example1");
+ *       private static final NarratorMode EXAMPLE2 =
+ *               addNarratorMode("EXAMPLE2", 5, "options.narrator.example2");
+ *       [...]
+ *
+ *       @Invoker("<init>")
+ *       public static NarratorMode narratorModeInvokeInit(
+ *               final String internalName,
+ *               final int internalId,
+ *               final int id,
+ *               final String name) {
+ *           throw new AssertionError();
+ *       }
+ *
+ *       private static NarratorMode addNarratorMode(final String internalName, final int id, final String name) {
+ *           final Object[] output = HELPER.addNarratorMode(VALUES, field_18183, internalName, id, name);
+ *
+ *           VALUES      = (NarratorMode[]) output[0];
+ *           field_18183 = (NarratorMode[]) output[1];
+ *           return        (NarratorMode)   output[2];
+ *       }
+ *   }
+ * }</pre>
+ *
+ * As well, since the "name" of a narrator mode appears to be a text translation key, language file(s) should be added
+ * just as they are added to fabric projects, under:
+ * <br/>
+ * <code>  * resources/assets/[modid]/lang/[language].json</code>.
+ * <p>
+ * Note: This class design is inspired by code from <a href="https://github.com/Micalobia">Micalobia</a> and
+ * <a href="https://github.com/LudoCrypt">LudoCrypt</a>.
+ *
+ * @author Case Walker
+ */
+public abstract class NarratorModeMixinHelperParent {
+
+    /**
+     * Invoke the init method of the {@link NarratorMode} enum.
+     *
+     * @param internalName Internal Java value, AFAIK
+     * @param internalId Internal Java value, AFAIK, related to {@link Enum#ordinal()}
+     * @param id See {@link #addNarratorMode(NarratorMode[], NarratorMode[], String, int, String)}
+     * @param name See {@link #addNarratorMode(NarratorMode[], NarratorMode[], String, int, String)}
+     * @return A newly created NarratorMode
+     */
+    protected abstract NarratorMode narratorModeInvokeInit(final String internalName,
+                                                           final int internalId,
+                                                           final int id,
+                                                           final String name);
+
+    /**
+     * This method is intended to be used to add a new value to the {@link NarratorMode} enum.
+     *
+     * @param VALUES The private member field of the {@link NarratorMode} enum
+     * @param field_18183 The Java-internal field used inside the enum
+     * @param internalName Used inside the {@link #narratorModeInvokeInit(String, int, int, String)} method
+     * @param id The numeric value ID for the new NarratorMode
+     * @param name The translatable name of the NarratorMode
+     * @return An Object array containing 1) the updated VALUES value, 2) the updated field_18183 value, and 3) the
+     * newly created NarratorMode
+     */
+    public Object[] addNarratorMode(
+            final NarratorMode[] VALUES,
+            final NarratorMode[] field_18183,
+            final String internalName,
+            final int id,
+            final String name) {
+        // In case the internal field and VALUES differ (this should not happen though), copy them separately
+        final List<NarratorMode> modesValues =
+                VALUES == null ? new ArrayList<>() : new ArrayList<>(Arrays.asList(VALUES));
+        final List<NarratorMode> modes_field_18183 =
+                field_18183 == null ? new ArrayList<>() : new ArrayList<>(Arrays.asList(field_18183));
+
+        // Only using field_18183 for ordinal
+        final int ordinal =
+                modes_field_18183.isEmpty() ? -1 : modes_field_18183.get(modes_field_18183.size() - 1).ordinal();
+        final NarratorMode customNarratorMode = narratorModeInvokeInit(internalName, ordinal + 1, id, name);
+
+        modesValues.add(customNarratorMode);
+        modes_field_18183.add(customNarratorMode);
+
+        final NarratorMode[] modesValuesArray = modesValues.toArray(new NarratorMode[0]);
+        final NarratorMode[] modes_field_18183_Array = modes_field_18183.toArray(new NarratorMode[0]);
+
+        return new Object[] {modesValuesArray, modes_field_18183_Array, customNarratorMode};
+    }
+}
---
+++ src/test/java/com/casewalker/modutils/util/NarratorModeMixinTest.java
@@ -0,0 +1,156 @@
+/*
+ * Licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2022 Case Walker.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.casewalker.modutils.util;
+
+import net.minecraft.client.option.NarratorMode;
+import net.minecraft.text.Text;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+/**
+ * Tests for the {@link NarratorModeMixinHelperParent}.
+ */
+class NarratorModeMixinTest {
+
+    /**
+     * Test class extending the {@link NarratorModeMixinHelperParent}.
+     */
+    private static class NarratorModeMixin extends NarratorModeMixinHelperParent {
+        protected NarratorMode narratorModeInvokeInit(String internalName, int internalId, int id, String name) {
+            // final class mocking made possible only by the MockMaker extension
+            final NarratorMode mock = Mockito.mock(NarratorMode.class);
+
+            when(mock.getId()).thenReturn(id);
+            when(mock.getName()).thenReturn(Text.of(name));
+            when(mock.name()).thenReturn(internalName);
+            when(mock.ordinal()).thenReturn(internalId);
+
+            return mock;
+        }
+    }
+
+    @Test
+    @DisplayName("Test adding a narrator mode successfully")
+    void addNewNarratorMode() {
+        NarratorMode[] VALUES = new NarratorMode[] {NarratorMode.ALL, NarratorMode.CHAT};
+        NarratorMode[] field_18183 = new NarratorMode[] {NarratorMode.ALL, NarratorMode.CHAT};
+        NarratorModeMixin mixin = new NarratorModeMixin();
+        String internalName = "TEST";
+        int id = 5;
+        String name = "options.narrator.test";
+
+        Object[] returned = mixin.addNarratorMode(VALUES, field_18183, internalName, id, name);
+
+        NarratorMode[] newVALUES = (NarratorMode[]) returned[0];
+        NarratorMode[] new_field_18183 = (NarratorMode[]) returned[1];
+        NarratorMode newMode = (NarratorMode) returned[2];
+        assertEquals(name, newMode.getName().asString(), "Name (Text) should match");
+        assertEquals(id, newMode.getId(), "ID should match");
+        assertEquals(internalName, newMode.name(), "Internal name should match");
+        assertEquals(field_18183[field_18183.length - 1].ordinal() + 1, newMode.ordinal(),
+                "Ordinal should be based on the last entry's ordinal of the field_18183 array");
+        assertEquals(newVALUES.length, new_field_18183.length, "Both arrays should have the same size");
+        assertEquals(newVALUES.length, VALUES.length + 1, "VALUES array should have one more element");
+        assertEquals(newMode, newVALUES[newVALUES.length - 1], "New element in VALUES should be the new mode");
+        assertEquals(newMode, new_field_18183[new_field_18183.length - 1],
+                "New element in field_18183 should be the new mode");
+    }
+
+    @Test
+    @DisplayName("Test adding dumb scenario if VALUES and field_18183 are different lengths")
+    void addNarratorModeDumbScenarioLengths() {
+        NarratorMode[] VALUES = new NarratorMode[] {NarratorMode.OFF};
+        NarratorMode[] field_18183 = new NarratorMode[] {NarratorMode.OFF, NarratorMode.SYSTEM};
+        NarratorModeMixin mixin = new NarratorModeMixin();
+        String internalName = "TEST";
+        int id = 5;
+        String name = "options.narrator.test";
+
+        Object[] returned = mixin.addNarratorMode(VALUES, field_18183, internalName, id, name);
+
+        NarratorMode[] newVALUES = (NarratorMode[]) returned[0];
+        NarratorMode[] new_field_18183 = (NarratorMode[]) returned[1];
+        NarratorMode newMode = (NarratorMode) returned[2];
+        assertEquals(id, newMode.getId(), "ID should match");
+        assertNotEquals(newVALUES.length, new_field_18183.length, "Arrays should be different sizes");
+        assertEquals(newVALUES.length, VALUES.length + 1, "VALUES array should have one more element");
+        assertEquals(new_field_18183.length, field_18183.length + 1, "field_18183 array should have one more element");
+        assertEquals(newMode, newVALUES[newVALUES.length - 1], "New element in VALUES should be the new mode");
+        assertEquals(newMode, new_field_18183[new_field_18183.length - 1],
+                "New element in field_18183 should be the new mode");
+    }
+
+    @Test
+    @DisplayName("Test adding a narrator mode if the underlying arrays start out empty")
+    void addNarratorModeEmptyArrays() {
+        NarratorMode[] VALUES = new NarratorMode[] {};
+        NarratorMode[] field_18183 = new NarratorMode[] {};
+        NarratorModeMixin mixin = new NarratorModeMixin();
+        String internalName = "TEST";
+        int id = 5;
+        String name = "options.narrator.test";
+
+        Object[] returned = mixin.addNarratorMode(VALUES, field_18183, internalName, id, name);
+
+        NarratorMode[] newVALUES = (NarratorMode[]) returned[0];
+        NarratorMode[] new_field_18183 = (NarratorMode[]) returned[1];
+        NarratorMode newMode = (NarratorMode) returned[2];
+        assertEquals(id, newMode.getId(), "ID should match");
+        assertEquals(newVALUES.length, new_field_18183.length, "Arrays should be the same size");
+        assertEquals(1, newVALUES.length, "VALUES array should have one element");
+        assertEquals(newMode, newVALUES[newVALUES.length - 1], "New element in VALUES should be the new mode");
+        assertEquals(newMode, new_field_18183[new_field_18183.length - 1],
+                "New element in field_18183 should be the new mode");
+    }
+
+    @Test
+    @DisplayName("Test adding a narrator mode if the underlying arrays start out null. This should never happen, but " +
+            "it may help having the logic present for testing and static initializations.")
+    void addNarratorModeNullArrays() {
+        NarratorModeMixin mixin = new NarratorModeMixin();
+        String internalName = "TEST";
+        int id = 5;
+        String name = "options.narrator.test";
+
+        Object[] returned = mixin.addNarratorMode(null, null, internalName, id, name);
+
+        NarratorMode[] newVALUES = (NarratorMode[]) returned[0];
+        NarratorMode[] new_field_18183 = (NarratorMode[]) returned[1];
+        NarratorMode newMode = (NarratorMode) returned[2];
+        assertEquals(id, newMode.getId(), "ID should match");
+        assertNotNull(newVALUES, "VALUES should not be null");
+        assertNotNull(new_field_18183, "field_18183 should not be null");
+        assertEquals(newVALUES.length, new_field_18183.length, "Arrays should be the same size");
+        assertEquals(1, newVALUES.length, "VALUES array should have one element");
+        assertEquals(newMode, newVALUES[newVALUES.length - 1], "New element in VALUES should be the new mode");
+        assertEquals(newMode, new_field_18183[new_field_18183.length - 1],
+                "New element in field_18183 should be the new mode");
+    }
+}
